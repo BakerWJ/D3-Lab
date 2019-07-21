b2129d94ea53bb5a574f69f33f9835dbab891c7d
Baker Jackson <jacba@utschools.ca>
2019-07-20 20:25:06 -0400

    tabs

--- static/js/scatter.js
+++ static/js/scatter.js
@@ -1,53 +1,53 @@
 'use strict';
 
 (function() {
-    let data = [];
-    d3.json("/load-data", (d) => {
-        return d;
-    }).then((d) => {
-        data = d['users'];
-        createVis();
-    }).catch((err) => {
-        console.error(err)
-    });
+	let data = [];
+	d3.json("/load-data", (d) => {
+		return d;
+	}).then((d) => {
+		data = d['users'];
+		createVis();
+	}).catch((err) => {
+		console.error(err)
+	});
 
-    function createVis() {
-        var nests = d3.nest()
-            .key((d) => {return d["hw1_hrs"];})
-            .key((d) => {return d["experience_yr"];})
-            .rollup((v) => {return v.length;})
-            .entries(data);
+	function createVis() {
+		var nests = d3.nest()
+			.key((d) => {return d["hw1_hrs"];})
+			.key((d) => {return d["experience_yr"];})
+			.rollup((v) => {return v.length;})
+			.entries(data);
 
-        var margin = 20,
-            width =400,
-            height = 300;
+		var margin = 20,
+			width =400,
+			height = 300;
 
-        var svg = d3.select("#scatter")
-            .append("svg")
-            .attr("width", width)
-            .attr("height", height);
+		var svg = d3.select("#scatter")
+			.append("svg")
+			.attr("width", width)
+			.attr("height", height);
 
-        const yMap = data.map(function (d, i) {
-            return d.hw1_hrs;
-        });
+		const yMap = data.map(function (d, i) {
+			return d.hw1_hrs;
+		});
 
-        const xMap = data.map((d, i) => {
-            return d.experience_yr;
-        });
+		const xMap = data.map((d, i) => {
+			return d.experience_yr;
+		});
 
-        const scX = d3.scaleLinear()
-            .domain(d3.extent(xMap, (d) => {
-                return d;
-            }))
-            .range([0, width]);
+		const scX = d3.scaleLinear()
+			.domain(d3.extent(xMap, (d) => {
+				return d;
+			}))
+			.range([0, width]);
 
-        const scY = d3.scaleLinear()
-            .domain(d3.extent(xMap, (d) => {
-                return d;
-            }))
-            .range([0, height]);
+		const scY = d3.scaleLinear()
+			.domain(d3.extent(xMap, (d) => {
+				return d;
+			}))
+			.range([0, height]);
 
 
 
-    }
+	}
 })();
